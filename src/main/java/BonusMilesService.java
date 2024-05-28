8f9196275f9794b4a42591d1302ebed00ca46fd7
Anastasya Biruk <anastasya.biruk7@gmail.com>
2024-05-28 17:29:02 +0300

    Initial commit

---
+++ src/main/java/BonusMilesService.java
@@ -0,0 +1,11 @@
+public class BonusMilesService {
+    public int calculate(int price) {
+        int bonus;
+        if (price >= 20) {
+            bonus = price / 20;
+        } else {
+            bonus = 0;
+        }
+        return bonus;
+    }
+}
